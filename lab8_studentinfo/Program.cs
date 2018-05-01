348a2cbd3ed589f1e17887b9c0c1932230bcd638
saunlani <saunlani@gmail.com>
2018-05-01 16:36:58 -0400

    refactored code, moved array declarartion upstream and utilized a method prompting the user to continue

--- lab8_studentinfo/Program.cs
+++ lab8_studentinfo/Program.cs
@@ -18,6 +18,19 @@ namespace lab8_studentinfo
 
         static void Main(string[] args)
         {
+            string[] Students = { "Joeseph", "Sean", "Stephen", "Drew", "Marco","Bruce", "Chris",
+                        "Dorian","Edward","Frank","Greg","Henry","Illya","Jim","Kendall","Lou","Mary",
+                        "Nancy","Othello","Paul" };
+            string[] Hometowns = { "Detroit, MI", "St. Clair Shores, MI", "Los Angeles, CA",
+                        "Roseville, MI", "Rome, Italy","Belleville, MI", "Chicago, IL", "Denver, CO",
+                        "Edmonton, Canada", "Frankenmuth, MI", "Gary, IN", "Hamburg, Germany",
+                        "Indianapolis, IN", "Jerusalem, Israel", "Kano, Nigeria", "La Trinidad, Nigeria",
+                        "Madison Heights, MI", "Nunapitchuk, Alaska", "Oakland, CA", "Pa Ju, South Korea"};
+            string[] FavFoods = { "Passion Fruit", "Hamburgers", "Tacos", "Pizza", "Lasagna","Soup",
+                        "Pizza","Chicken Schwarma","Pad Thai","Hot Dogs","Apples","Potatoes","Drunken Noodle","Chips","Salad",
+                        "Sandwiches","Bibimbap","Chicken Vindaloo","Lamb Korma","Jinga Masala"};
+
+
             Console.WriteLine("Welcome to our C# class! Which student would you like to learn more about?");
 
             bool RunProgram = true;
@@ -31,77 +44,37 @@ namespace lab8_studentinfo
                 int.TryParse(userchoice, out studentnum);
                 studentnum = studentnum - 1;
 
-                if (studentnum < 0 || studentnum >= 20 )
+                if (studentnum < 0 || studentnum >= 20)
                 {
                     Console.WriteLine("That student does not exist.  Please try again.");
                     continue;
                 }
                 else
                 {
-                    string[] Students = { "Joeseph", "Sean", "Stephen", "Drew", "Marco","Bruce", "Chris",
-                        "Dorian","Edward","Frank","Greg","Henry","Illya","Jim","Kendall","Lou","Mary",
-                        "Nancy","Othello","Paul" };
-                    string[] Hometowns = { "Detroit, MI", "St. Clair Shores, MI", "Los Angeles, CA", 
-                        "Roseville, MI", "Rome, Italy","Belleville, MI", "Chicago, IL", "Denver, CO", 
-                        "Edmonton, Canada", "Frankenmuth, MI", "Gary, IN", "Hamburg, Germany", 
-                        "Indianapolis, IN", "Jerusalem, Israel", "Kano, Nigeria", "La Trinidad, Nigeria", 
-                        "Madison Heights, MI", "Nunapitchuk, Alaska", "Oakland, CA", "Pa Ju, South Korea"};
-                    string[] FavFoods = { "Passion Fruit", "Hamburgers", "Tacos", "Pizza", "Lasagna","Soup",
-                        "Pizza","Chicken Schwarma","Pad Thai","Hot Dogs","Apples","Potatoes","Drunken Noodle","Chips","Salad",
-                        "Sandwiches","Bibimbap","Chicken Vindaloo","Lamb Korma","Jinga Masala"};
                     Console.WriteLine($"Student {studentnum + 1} is {Students[studentnum]}. What would you like to know about {Students[studentnum]}?");
+                    Console.WriteLine("Enter 'hometown' or 'favorite food'.");
+                    string infochoice = Console.ReadLine();
 
-                    bool goagain = true;
-                    while (goagain == true)
+                    if (infochoice != "hometown" && infochoice != "favorite food")
                     {
-                        Console.WriteLine("Enter 'hometown' or 'favorite food'.");
-                        string infochoice = Console.ReadLine();
-
-                        if (infochoice != "hometown" && infochoice != "favorite food")
-                        {
-                            Console.WriteLine("That data does not exist.  Please try again.");
-                            continue;
-                        }
-                        else if (infochoice == "hometown")
-                        {
-                            Console.WriteLine($"{Students[studentnum]} is from {Hometowns[studentnum]}.");
-
-                            Console.WriteLine("Would you like to know more?  Type y for yes or anything else for no.");
-                            string input = Console.ReadLine();
-                            if (input == "y")
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Bye!");
-                                goagain = false;
-                                RunProgram = false;
-                            }
-
-                        }
-
-                        else if (infochoice == "favorite food")
-                        {
-                            Console.WriteLine($"{Students[studentnum]}'s favorite food is {FavFoods[studentnum]}.");
-
-                            Console.WriteLine("Would you like to know more?  Type y for yes or anything else for no.");
-                            string input = Console.ReadLine();
-                            if (input == "y")
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Bye!");
-                                goagain = false;
-                                RunProgram = false;
-                            }
-                        }
+                        Console.WriteLine("That data does not exist.  Please try again.");
+                        continue;
                     }
+
+                    else if (infochoice == "hometown")
+                    {
+                        Console.WriteLine($"{Students[studentnum]} is from {Hometowns[studentnum]}.");
+                        RunProgram = GoAgainAsker();
+                    }
+                    else if (infochoice == "favorite food")
+
+                    {
+                        Console.WriteLine($"{Students[studentnum]}'s favorite food is {FavFoods[studentnum]}.");
+                        RunProgram = GoAgainAsker();
+                    }  
                 }
             }
- 
+
         }
 
         static bool GoAgainAsker()
@@ -111,10 +84,11 @@ namespace lab8_studentinfo
             if (input == "y")
             {
                 return true;
-            
+
             }
             else
             {
+                Console.WriteLine("Bye!");
                 return false;
             }
         }
@@ -122,4 +96,3 @@ namespace lab8_studentinfo
 
     }
 }
-
