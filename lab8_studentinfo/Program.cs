279b748e55b5db3167b973f1140e4bf82323013b
saunlani <saunlani@gmail.com>
2018-05-01 15:50:11 -0400

    added 15 more students, changed array info to reflect fav food

--- lab8_studentinfo/Program.cs
+++ lab8_studentinfo/Program.cs
@@ -23,7 +23,7 @@ namespace lab8_studentinfo
             bool RunProgram = true;
             while (RunProgram == true)
             {
-                Console.WriteLine("Enter a student number 1-5");
+                Console.WriteLine("Enter a student number 1-20");
 
                 string userchoice;
                 userchoice = Console.ReadLine();
@@ -31,25 +31,33 @@ namespace lab8_studentinfo
                 int.TryParse(userchoice, out studentnum);
                 studentnum = studentnum - 1;
 
-                if (studentnum < 0 || studentnum >= 5 )
+                if (studentnum < 0 || studentnum >= 20 )
                 {
                     Console.WriteLine("That student does not exist.  Please try again.");
                     continue;
                 }
                 else
                 {
-                    string[] Students = { "Joeseph", "Sean", "Stephen", "Drew", "Marco" };
-                    string[] Hometowns = { "Detroit", "St. Clair Shores", "Los Angeles", "Roseville", "Rome" };
-                    string[] FavBands = { "Hiatus Kaiyote", "Protomartyr", "Odesza", "Radiohead", "Tool" };
+                    string[] Students = { "Joeseph", "Sean", "Stephen", "Drew", "Marco","Bruce", "Chris",
+                        "Dorian","Edward","Frank","Greg","Henry","Illya","Jim","Kendall","Lou","Mary",
+                        "Nancy","Othello","Paul" };
+                    string[] Hometowns = { "Detroit, MI", "St. Clair Shores, MI", "Los Angeles, CA", 
+                        "Roseville, MI", "Rome, Italy","Belleville, MI", "Chicago, IL", "Denver, CO", 
+                        "Edmonton, Canada", "Frankenmuth, MI", "Gary, IN", "Hamburg, Germany", 
+                        "Indianapolis, IN", "Jerusalem, Israel", "Kano, Nigeria", "La Trinidad, Nigeria", 
+                        "Madison Heights, MI", "Nunapitchuk, Alaska", "Oakland, CA", "Pa Ju, South Korea"};
+                    string[] FavFoods = { "Passion Fruit", "Hamburgers", "Tacos", "Pizza", "Lasagna","Soup",
+                        "Pizza","Chicken Schwarma","Pad Thai","Hot Dogs","Apples","Potatoes","Drunken Noodle","Chips","Salad",
+                        "Sandwiches","Bibimbap","Chicken Vindaloo","Lamb Korma","Jinga Masala"};
                     Console.WriteLine($"Student {studentnum + 1} is {Students[studentnum]}. What would you like to know about {Students[studentnum]}?");
 
                     bool goagain = true;
                     while (goagain == true)
                     {
-                        Console.WriteLine("Enter 'hometown' or 'favorite band'.");
+                        Console.WriteLine("Enter 'hometown' or 'favorite food'.");
                         string infochoice = Console.ReadLine();
 
-                        if (infochoice != "hometown" && infochoice != "favorite band")
+                        if (infochoice != "hometown" && infochoice != "favorite food")
                         {
                             Console.WriteLine("That data does not exist.  Please try again.");
                             continue;
@@ -73,9 +81,9 @@ namespace lab8_studentinfo
 
                         }
 
-                        else if (infochoice == "favorite band")
+                        else if (infochoice == "favorite food")
                         {
-                            Console.WriteLine($"{Students[studentnum]}'s favorite band is {FavBands[studentnum]}.");
+                            Console.WriteLine($"{Students[studentnum]}'s favorite food is {FavFoods[studentnum]}.");
 
                             Console.WriteLine("Would you like to know more?  Type y for yes or anything else for no.");
                             string input = Console.ReadLine();
